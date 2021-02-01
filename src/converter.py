a6cc408cf6f78bf9bd0b95cc9f770afdca648398
Andres Galvan <andresgalfajar@gmail.com>
2021-02-01 09:55:36 -0500

    Reduce the code by reusing similar math processes in functions and accept the 0 in the hex input

--- src/converter.py
+++ src/converter.py
@@ -1,7 +1,11 @@
 from time import time
 from subprocess import call
 from re import fullmatch
-from src.controllers.controllers import *
+try:
+    from src.controllers.controllers import *
+except:
+    from controllers.controllers import *
+
 if OS == "Windows":
     from msvcrt import getche, getch
 else:
@@ -10,6 +14,7 @@ else:
 
 
 def keep_doing():
+    print("\n\nEsc para salir...")
     key_pressed = ord(getch())
     clear()
     if key_pressed == 27:
@@ -17,42 +22,73 @@ def keep_doing():
     return 1
 
 
-def decimal2binary():
-    clear()
-    center_print("DECIMAL A BINARIO", 5)
+def convert_by_division(base: int):
     decimal = ask_value(8, int)
 
-    x_position = 2
     y_position = 10
-    binary_list = []
+    converted_list = []
+    spaces = len(str(decimal))
     while decimal >= 1:
-        gotoxy(x_position, y_position)
-        operation = f"{decimal} │ 2"
-        print(operation)
+        binary_digit = decimal % base
+        converted_list.append(binary_digit)
+
+        operation = f"{decimal:>{spaces}} │ {base} -> {binary_digit}"
+        center_print(operation, y_position)
         y_position += 1
 
-        bin_digit = decimal % 2
-        spaces = len(str(decimal))
-        decimal //= 2
-        gotoxy(x_position, y_position)
-        print("{0}{1}└───┐".format(bin_digit, " " * spaces))
-        binary_list.append(bin_digit)
+        decimal //= base
 
+    return converted_list[::-1], y_position
+
+
+def convert_by_multiplication(iterable, base: int):
+    y_position = 10
+    decimal_number = 0
+    for power, n in enumerate(iterable[::-1]):
+        power_result = base ** power
+        dec_acumulator = int(n) * power_result
+        decimal_number += dec_acumulator
+        operation = f"{n} x {base}^{power} = {n} x {power_result} = {dec_acumulator}"
+        center_print(operation, y_position)
         y_position += 1
-        x_position += 4
-    gotoxy(x_position, y_position)
+
+    bar = "─" * (len(str(decimal_number)) + 2)
+    center_print(bar, y_position)
+    center_print(str(decimal_number), y_position + 1)
+
+
+def decimal_to_binary():
+    clear()
+    center_print("DECIMAL A BINARIO", 5)
+    binary_base = 2
+    binary_list, y_position = convert_by_division(binary_base)
+
+    binary_number = "".join([str(n) for n in binary_list])
 
     center_print("Número binario.", y_position+2)
-    binary_number = ""
-    for n in binary_list[::-1]:
-        binary_number += str(n)
     center_print(binary_number, y_position+3)
-    print()
 
     return keep_doing()
 
 
-def binary2decimal():
+def decimal_to_hex():
+    clear()
+    center_print("DECIMAL A HEXADECIMAL", 5)
+    hexadecimal_base = 16
+    hexadecimal_list, y_position = convert_by_division(hexadecimal_base)
+
+    specials_hex = {"10": "A", "11": "B", "12": "C",
+                    "13": "D", "14": "E", "15": "F"}
+
+    center_print("Número hexadecimal.", y_position+2)
+    hexadecimal_number = "".join([str(n) if n < 10 else specials_hex[str(n)]
+                                  for n in hexadecimal_list])
+    center_print(hexadecimal_number, y_position+3)
+
+    return keep_doing()
+
+
+def binary_to_decimal():
     clear()
     center_print("BINARIO A DECIMAL", 5)
     while True:
@@ -62,68 +98,19 @@ def binary2decimal():
         error_print("Sólo ingrese 1-0", "c", 10)
         clear_input(23, 8)
 
-    y_position = 10
-    decimal_number = 0
-    for power, n in enumerate(str(binary)[::-1]):
-        power_result = 2 ** power
-        dec_acumulator = int(n) * power_result
-        decimal_number += dec_acumulator
-        operation = f"{n} x 2 ^{power} = {n} x {power_result} = {dec_acumulator}"
-        center_print(operation, y_position)
-        y_position += 1
+    binary = str(binary)
+    binary_base = 2
+    convert_by_multiplication(binary, binary_base)
 
-    bar = "─" * (len(str(decimal_number)) + 2)
-    center_print(bar, y_position)
-    center_print(str(decimal_number), y_position + 1)
-    print()
     return keep_doing()
 
 
-def decimal2hex():
-    clear()
-    center_print("DECIMAL A HEXADECIMAL", 5)
-    decimal = ask_value(8, int)
-
-    x_position = 2
-    y_position = 10
-    hexadecimal_list = []
-    while decimal >= 1:
-        gotoxy(x_position, y_position)
-        operation = f"{decimal} │ 16"
-        print(operation)
-        y_position += 1
-
-        hex_digit = decimal % 16
-        spaces = len(str(decimal))
-        decimal //= 16
-        gotoxy(x_position, y_position)
-        print("{0}{1}└──┐".format(hex_digit, " " * spaces))
-        hexadecimal_list.append(hex_digit)
-
-        y_position += 1
-        x_position += 4
-    gotoxy(x_position, y_position)
-
-    center_print("Número hexadecimal.", y_position+2)
-    specials_hex = {"10": "A", "11": "B", "12": "C",
-                    "13": "D", "14": "E", "15": "F"}
-    hexadecimal_number = ""
-    for n in hexadecimal_list[::-1]:
-        if n >= 10:
-            hexadecimal_number += specials_hex[str(n)]
-            continue
-        hexadecimal_number += str(n)
-    center_print(hexadecimal_number, y_position+3)
-    print()
-    return keep_doing()
-
-
-def hex2decimal():
+def hex_to_decimal():
     clear()
     center_print("HEXADECIMAL A DECIMAL", 5)
     while True:
-        hexadecimal = ask_value(8, str).upper()
-        if fullmatch("[1-9ABCDEF]+", str(hexadecimal)):
+        hexadecimal = ask_value(8).upper()
+        if fullmatch("[0-9ABCDEF]+", str(hexadecimal)):
             break
         error_print("Valores inválidos.", "c", 10)
         clear_input(23, 8)
@@ -134,22 +121,15 @@ def hex2decimal():
     hexa_list = [value for value in hexadecimal]
     hexa_list = [specials_hex[n] if not n.isdigit() else n for n in hexa_list]
 
-    y_position = 10
-    decimal_number = 0
-    for power, n in enumerate(hexa_list[::-1]):
-        power_result = 16 ** power
-        hex_acumulator = int(n) * power_result
-        decimal_number += hex_acumulator
-        operation = f"{n} x 2 ^{power} = {n} x {power_result} = {hex_acumulator}"
-        center_print(operation, y_position)
-        y_position += 1
+    hexa_base = 16
+    convert_by_multiplication(hexa_list, hexa_base)
 
-    bar = "─" * (len(str(decimal_number)) + 2)
-    center_print(bar, y_position)
-    center_print(str(decimal_number), y_position + 1)
-    print()
     return keep_doing()
 
 
+def hex_to_binary():
+    hex_to_decimal()
+
+
 if __name__ == "__main__":
-    hex2decimal()
+    hex_to_decimal()
